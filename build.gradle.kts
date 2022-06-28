455831c0e4b25a92f5cb411c1637253bcc6597c1
leleliu008 <leleliu008@gmail.com>
2022-06-28 17:26:44 +0800

    optimized

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,11 +8,11 @@ buildscript {
     }
 
     dependencies {
-        //用于构建Android工程的插件
+        //用于构建出apk/aar的插件
         //https://developer.android.google.cn/studio/releases/gradle-plugin
         classpath("com.android.tools.build:gradle:7.1.3")
 
-        //Kotlin编译的插件
+        //用于编译Kotlin代码的插件
         //http://kotlinlang.org/docs/reference/using-gradle.html
         classpath(kotlin("gradle-plugin", kotlinVersion))
     }
